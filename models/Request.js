75e5028b8ce314ac347e188dbb0e2f4733b85c09
tommy2804 <t1o2mm3y@gmail.com>
2023-01-26 10:47:23 +0200

    tommy

---
+++ models/Request.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const requestSchema = mongoose.Schema(
+  {
+    reqNumber: { type: String, required: true },
+    reqPhoto: Buffer,
+    ofUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    ergency: { type: Number, min: 1, max: 10, default: 1 },
+    status: { type: String, default: 'sent' },
+    reqStreet: String,
+    reStreetNum: Number,
+    reqIsDone: { type: Boolean, default: false },
+    reqDescription: { type: String, maxLength: 255 },
+    reqTitle: String,
+    inCharge: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Request', requestSchema);
